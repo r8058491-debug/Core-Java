5392b4c12bc29d3dc11e1b24a24d267b92f7a410
r8058491-debug <r8058491@gmail.com>
2025-08-26 23:14:58 +0530

    Operators

---
+++ absolutenumber.java
@@ -0,0 +1,12 @@
+package operator_ques;
+
+public class absolutenumber {
+	public static void main(String[]args) {
+		int a=-5;
+		int b=3;
+		int c=-19;
+		int sum=(a+b+c);
+		System.out.println("Sum :"+sum);
+	}
+
+}
---
+++ checkdivisibility.java
@@ -0,0 +1,13 @@
+package operator_ques;
+
+public class checkdivisibility {
+	public static void main(String[]args) {
+		int a=45;
+		if(a%3==0 && a%5==0) {
+			System.out.println("Yes, It is divisible by both");
+		}
+		else {
+			System.out.println("No, It is not divisible by both");
+		}
+		}
+}
