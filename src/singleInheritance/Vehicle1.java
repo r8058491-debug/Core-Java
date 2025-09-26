2e8f7fc4cdaa553df8fbebac3edc894a7acde8d6
r8058491-debug <r8058491@gmail.com>
2025-09-26 18:04:41 +0530

    push

---
+++ src/singleInheritance/Vehicle1.java
@@ -0,0 +1,13 @@
+package singleInheritance;
+public class Vehicle1 {
+	String brand;
+	
+	Vehicle1(String brand){
+		this.brand=brand;
+	}
+	
+	void start() {
+		System.out.println(brand+"is starting.");
+	}
+
+}
