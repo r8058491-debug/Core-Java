535696e74df106a93b605bbf60426f1322d534a6
r8058491-debug <r8058491@gmail.com>
2025-11-28 09:42:01 +0530

    File Writer

--- src/file_handling/FileWriterExample.java
+++ src/file_handling/FileWriterExample.java
@@ -1,29 +1,29 @@
-package file_handling;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
-public class FileWriterExample {
-	public static void main(String[]args) {
-		try {
-			BufferedReader fw=new BufferedReader(new FileReader("example.txt"));
-			String line;
-			int countWord;
-			int countLine;
-			while(line=fw.readLine()!= null) {
-				System.out.println(line);
-				
-			}
-			
-			
-			
-		}catch(IOException e) {
-			System.out.println("An error occured..");
-			e.printStackTrace();
-		}
-		
-		
-	}
-}
+//package file_handling;
+//
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//
+//public class FileWriterExample {
+//	public static void main(String[]args) {
+//		try {
+//			BufferedReader fw=new BufferedReader(new FileReader("example.txt"));
+//			String line;
+//			int countWord;
+//			int countLine;
+////			while(line=fw.readLine()!= null) {
+//				System.out.println(line);
+//				
+//			}
+//			
+//			
+//			
+//		}catch(IOException e) {
+//			System.out.println("An error occured..");
+//			e.printStackTrace();
+//		}
+//		
+//		
+//	}
+//}
---
+++ src/neeraj/InvalidAmountException.java
@@ -0,0 +1,8 @@
+package neeraj;
+
+public class InvalidAmountException extends Exception{
+	public InvalidAmountException(String message) {
+		super(message);
+	}
+
+}
