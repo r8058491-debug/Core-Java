01e81608ad2d5de3258ac64c772ab699c5db5c37
r8058491-debug <r8058491@gmail.com>
2025-12-02 20:05:36 +0530

    Update File Handling Questions

--- src/FileHandlingQuestions/CountLinesnParagraph.java
+++ src/FileHandlingQuestions/CountLinesnParagraph.java
@@ -6,6 +6,8 @@ public class CountLinesnParagraph {
 			FileWriter fw=new FileWriter("Text.txt");
 			fw.write("Hello Guys");
 			fw.write("\nOk then We will Meet Again...");
+			fw.write("\n");
+			fw.write("\nHELLO");
 			fw.close();
 			BufferedReader br=new BufferedReader(new FileReader("Text.txt"));
 			String line;
@@ -13,9 +15,13 @@ public class CountLinesnParagraph {
 			int countParagraph=0;
 			while((line=br.readLine())!=null) {
 				System.out.println(line);
+				if(line.isBlank()) {
+					countParagraph++;
+				}
 				countLine++;
 			}
 			System.out.println("Total Lines :"+countLine);
+			System.out.println("Total Paragraph :"+countParagraph);
 			
 			br.close();
 		}catch(IOException e) {
--- src/FileHandlingQuestions/ReplaceWord.java
+++ src/FileHandlingQuestions/ReplaceWord.java
@@ -1,19 +1,23 @@
 package FileHandlingQuestions;
 import java.io.*;
-import java.util.*;
 public class ReplaceWord {
 	public static void main(String[]args) {
-		Scanner src=new Scanner(System.in);
 		try {
 			BufferedReader br=new BufferedReader(new FileReader("Text.txt"));
-			BufferedWriter bw=new BufferedWriter(new FileWriter("output.txt",true));
+			BufferedWriter bw=new BufferedWriter(new FileWriter("output.txt"));
 			String line;
-			System.out.println("Enter the Word :");
-			String word=src.nextLine();
-			System.out.println("Enter the Replace word :");
-			String replace=src.nextLine();
-			while
 
+			while((line=br.readLine())!=null) {
+				line=line.replace("Hello", "Hi");
+				bw.write(line);
+				bw.newLine();
+			}
+			br.close();
+			bw.close();
+			System.out.println("Replaced Successfully in new File...");
+
+		}catch(IOException e) {
+			System.out.println("Error :"+e.getStackTrace());
 		}
 	}
 
