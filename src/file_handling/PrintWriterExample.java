84e3ba08e8bf3bd13700d43607890d066521a20f
r8058491-debug <r8058491@gmail.com>
2025-12-02 21:27:26 +0530

    Updated PrintWriter

--- src/file_handling/PrintWriterExample.java
+++ src/file_handling/PrintWriterExample.java
@@ -1,14 +1,24 @@
 package file_handling;
 import java.io.*;
+import java.util.Scanner;
 public class PrintWriterExample {
 	public static void main(String[]args) {
 		try {
-			PrintWriter pw=new PrintWriter("output.txt");
-			pw.println("Name :Rajat");
-			pw.println("Class : 8");
-			pw.println("Subject : Math");
-			pw.printf("Score :%d%%",95);
+			PrintWriter pw=new PrintWriter("student.txt");
+			pw.println("Rajat 8 Math 95");
+			pw.println("Rohan 9 Science 90");
+			pw.println("Rishi 10 English 77");
+			pw.println("Akshay 11 Physics 96");
 			pw.close();
+			Scanner src=new Scanner(new File("student.txt"));
+			while(src.hasNext()) {
+				String name=src.next();
+				int cclass=src.nextInt();
+				String subject=src.next();
+				int marks=src.nextInt();
+				System.out.println("Name :"+name+" | Class :"+cclass+"th"+"| Subject :"+subject+"| Marks :"+marks+"%");
+			}
+			src.close();
 		}catch(IOException e) {
 			e.getStackTrace();
 		}
