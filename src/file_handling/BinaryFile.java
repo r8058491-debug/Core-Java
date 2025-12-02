8e38fe5afefe8b29c1e25b144244903d1662e9d8
r8058491-debug <r8058491@gmail.com>
2025-12-02 21:07:29 +0530

    Binary File

--- src/file_handling/BinaryFile.java
+++ src/file_handling/BinaryFile.java
@@ -2,12 +2,18 @@ package file_handling;
 import java.io.*;
 public class BinaryFile {
 	public static void main(String[]args) {
-		try(FileInputStream fis=new FileInputStream("");
-			FileOutputStream fos=new FileOutputStream(" ")) {
+		try(FileInputStream fis=new FileInputStream("C:\\Users\\RAJAT\\OneDrive\\Binary File\\nature-yuh.png");
+			FileOutputStream fos=new FileOutputStream("C:\\Users\\RAJAT\\OneDrive\\Attachments\\nature.png")) {
 			
 			int byteData;
-			
-			
+			while((byteData=fis.read())!=-1) {
+				fos.write(byteData);
+			}
+			System.out.println("Binary Copied Successfully");
+			fis.close();
+			fos.close();
+		}catch(IOException e) {
+			System.out.println("Error :"+e.getStackTrace());
 		}
 	}
 
